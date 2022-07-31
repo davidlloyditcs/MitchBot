b2d1b654630f31f76029aa60079f52e38b76daeb
Jeff Kim <jeffersontaekim@gmail.com>
2022-07-31 12:20:17 -0700

    Fix token issue

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
-require("dotenv").config();
-const Discord = require('discord.js');
-const client = new Client({intents: ['GUILDS', 'GUILD_MESSAGES']});
 
-client.on("ready", () => {
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once("ready", () => {
     console.log("BOT IS ONLINE");
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return; 
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'beep') {
+        await interaction.reply('Boop!');
     }
 });
-client.login(process.env.DISCORD_TOKEN);
+
+client.login(token);
